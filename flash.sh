3babfb4a4bf036a3cbd4ef8569b8be56b5aab1c3
JohnnyNiu <johnnyniu@aconex.com>
2017-11-22 07:23:50 +1100

    Calculate serial address when flashing. As it may change

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-esptool.py -p /dev/tty.wchusbserial14510 -b 230400 write_flash -ff 40m -fm qio -fs 4m 0x00000 binaries/0x00000.bin 0x0a000 binaries/0x0a000.bin 0x4A000 binaries/spiff_rom.bin
+esptool.py -p $(ls /dev/tty.wchusbserial*) -b 230400 write_flash -ff 40m -fm qio -fs 4m 0x00000 binaries/0x00000.bin 0x0a000 binaries/0x0a000.bin 0x4A000 binaries/spiff_rom.bin
